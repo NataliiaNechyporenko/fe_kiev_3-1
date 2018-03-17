ce9284339cba83c5489bcf84285d558013065ff5
Nataliia Nechiporenko <nataliia.nechiporenko@gmail.com>
2018-03-17 17:46:52 +0200

    Fixed bugs with wrong logic

--- nechyporenko-n_js-m14/script.js
+++ nechyporenko-n_js-m14/script.js
@@ -2,22 +2,29 @@ let startBtn = document.querySelector("#start_btn"),
     stopBtn = document.querySelector("#stop_btn");
 
 function Timer () {
-    this.startTime = this.start;
-    this.stopTime = this.stop;
-    this.interval = (this.stopTime - this.startTime)/1000;
+    this.startTime = 0;
+    this.stopTime = 0;
+    this.interval = 0;
 };
 
+Timer.prototype.start = function() {
+    this.startTime = Date.now();
+    };
+
+Timer.prototype.stop = function() {
+    this.stopTime = Date.now();
+    timer.interval = (timer.stopTime - timer.startTime)/1000;
+    console.log(`Interval: ${timer.interval} sec`);
+    };
+
+const timer = new Timer ();
+
 let clickHandler = (event) => {
-    if (event.target == startBtn) {
-        console.log(`Start`);
-        Timer.prototype.start = Date.now();
+    if (event.target === startBtn) {
+        timer.start();
     }
-    if (event.target == stopBtn) {
-        console.log(`Stop`);
-        Timer.prototype.stop = Date.now();
-        let newTimer = new Timer();
-        console.log(newTimer);
-        console.log(`Timer interval = ${newTimer.interval} sec`);
+    if (event.target === stopBtn) {
+        timer.stop();
     };
 };
 
